1c0351b3b2b887f452a3062b88b157255d5648f8
bitshubham-31 <ashubhamz321@gmail.com>
2026-03-31 23:48:37 +0530

    add

--- python.py
+++ python.py
@@ -1,5 +1,6 @@
-Python 3.14.3 (v3.14.3:323c59a5e34, Feb  3 2026, 11:41:37) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
-Enter "help" below or click "Help" above for more information.
->>> print("hello world")
-hello world
+print("hello world")
 print("my first")
+a=12
+b=15
+add=a+b
+print("the sum is",add)
